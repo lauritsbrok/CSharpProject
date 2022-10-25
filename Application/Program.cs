e2a32d0679d261015cd126704d85d9c67b9feef1
DanielNygaard <94904320+DanielNygaard@users.noreply.github.com>
2022-10-25 17:43:30 +0200

    Mit forsøg på at lave tests

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+namespace Application
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+        }
+
+        public static IEnumerable<String> CommitFrequency(String path)
+        {
+            return new List<String>();
+        }
+
+        public static IEnumerable<String> CommitAuthor(String path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
--- ApplicationTest/UnitTest1.cs
+++ ApplicationTest/UnitTest1.cs
@@ -1,10 +1,50 @@
-namespace ApplicationTest;
+namespace Application.Tests;
+using Application;
 
 [TestClass]
 public class UnitTest1
 {
-    [TestMethod]
-    public void TestMethod1()
+    [Fact]
+    public void FrequencyTest()
     {
+        // Given
+        var path = "PathToGit";
+        var expectedOutput = new List<String>(){
+            "1 2017-12-08",
+            "6 2017-12-26",
+            "12 2018-01-01",
+            "13 2018-01-02",
+            "10 2018-01-14",
+            "7 2018-01-17",
+            "5 2018-01-18"
+        };
+        // When
+        var actualOutput = Program.CommitFrequency(path);
+
+        // Then
+        expectedOutput.Should().BeEquivalentTo(actualOutput);
+    }
+
+    [Fact]
+    public void AutherTest()
+    {
+        // Given
+        var path = "PathToGit";
+        var expectedOutput = new List<String>(){
+        "Marie Beaumin",
+            "1 2017-12-08",
+            "6 2017-12-26",
+            "12 2018-01-01",
+            "13 2018-01-02",
+            "10 2018-01-14",
+            "7 2018-01-17",
+            "5 2018-01-18"
+        };
+
+        // When
+        var actualOutput = Program.CommitFrequency(path);
+
+        // Then
+        expectedOutput.Should().BeEquivalentTo(actualOutput);
     }
 }
