fb68e0aa219891f1dca564f163b1c3ca5b3e47f0
Laurits Brok <lauritsbrok@gmail.com>
2022-10-28 17:16:41 +0200

    Updated method

--- Application/Commits.cs
+++ Application/Commits.cs
@@ -4,12 +4,27 @@ using LibGit2Sharp;
 using LibGit2Sharp.Core;
 
 public class Commits {
-    
-    public static IEnumerable<String> getCommitsFrequency(){
-        var repo = new Repository("/Users/lauritsbrok/Documents/Drev/ITU/Analysis, Design and Software Architecture/assignment-05");
+
+    public static Dictionary<String, Dictionary<String, int>> getCommitsFrequency(Repository repo){
         var commits = repo.Commits;
+
+        var commitsByNameAndDateAndCount = new Dictionary<String, Dictionary<String, int>>();
+
         foreach(Commit a in commits){
-            yield return a.MessageShort;
+            var commitName = a.Committer.Name;
+            var commitDate = a.Committer.When.Date.ToString().Replace(" 00.00.00", "").Replace(".", "-");
+
+            Dictionary<String, int> currentDict;
+            if(!commitsByNameAndDateAndCount.TryGetValue(commitName, out currentDict)){
+                var dict = new Dictionary<String, int>();
+                dict.Add(commitDate, 1);
+                commitsByNameAndDateAndCount.Add(commitName, dict);
+            }
+
+            int count;
+            commitsByNameAndDateAndCount[commitName].TryGetValue(commitDate, out count);
+            commitsByNameAndDateAndCount[commitName][commitDate] = count + 1;
         }
+        return commitsByNameAndDateAndCount;
     }
 }
--- Application/Program.cs
+++ Application/Program.cs
@@ -1,13 +1,22 @@
-﻿namespace Application;
+﻿using LibGit2Sharp;
+
+namespace Application;
 
 public class TestClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine(args[0]);
-        var commits = Commits.getCommitsFrequency();
-        foreach(String a in commits){
-            Console.WriteLine(a);
+        var repo = new Repository("/Users/lauritsbrok/Documents/Drev/ITU/Analysis, Design and Software Architecture/assignment-05");
+        var dict = Commits.getCommitsFrequency(repo);
+
+        foreach(var a in dict)
+        {
+            Console.WriteLine(a.Key);
+            foreach(var b in a.Value){
+                Console.WriteLine("      " + b.Value.ToString() + " " + b.Key);
+            }
+            Console.WriteLine();
+
         }
 
     }
