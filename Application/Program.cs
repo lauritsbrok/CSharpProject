d71eb98a150c032d533730e40783a6b5846e2c51
Christoffer <hecn@itu.dk>
2022-10-28 13:08:14 +0200

    We did good today, CommitFrequencyMode works with test

--- Application/Program.cs
+++ Application/Program.cs
@@ -3,28 +3,29 @@
 public class Program{
 
     public static void Main(String[] args){
-        Console.WriteLine("Start");
-        CommitFrequencyMode(@"C:\Users\chris\Desktop\IT-Universitet\Semester 3\Analysis, Design and Software Architecture\assignment-01");
-        Console.WriteLine("Done");
-        // Console.WriteLine("Enter github repo path");
-        // var path = Console.ReadLine();
-        // Console.WriteLine("Choose desired info");
-        // Console.WriteLine("1) Commit Frequency Mode");
-        // Console.WriteLine("2) Commit Frequency Mode");
-        // int chosenMode = Console.Read();
-        // switch (chosenMode) {
-        //     case 1:
-        //     CommitFrequencyMode(@"C:\Users\chris\Desktop\IT-Universitet\Semester 3\Analysis, Design and Software Architecture\assignment-01");
-        //     break;
-        //     case 2:
-        //     CommitAuthorMode(path);
-        //     break;
-        // }
+        Console.WriteLine("Enter github repo path");
+        var path = Console.ReadLine();
+        Console.WriteLine("Choose desired info");
+        Console.WriteLine("1) Commit Frequency Mode");
+        Console.WriteLine("2) Commit Frequency Mode");
+        int chosenMode = int.Parse(Console.ReadLine()!);
+        switch (chosenMode) {
+            case 1:
+            var commitList = CommitFrequencyMode(path!);
+            foreach (String commit in commitList) {
+                Console.WriteLine(commit);
+            }
+            break;
+            case 2:
+            CommitAuthorMode(path!);
+            break;
+        }
     }
 
     public static IEnumerable<String> CommitFrequencyMode(String path){
         if(Repository.IsValid(path)){
-            var commitDataList = new Dictionary<int, DateTimeOffset>();
+            var commitDict = new Dictionary<String, int>();
+            var listCommit = new List<String>();
             using (var repo = new Repository(path))
                 {
                 Console.WriteLine(Repository.IsValid(path));
@@ -33,14 +34,16 @@ public class Program{
                     .Select(x => x.First())
                     .ToArray();
                 foreach (var commit in commits) {
-                    var commitName = commit;
-                    var commitDate = commit.Author.When;
-                    
-                //commitDataList.Add();
-                Console.WriteLine(commit + " " + commitDate + " " + commit.Author.Name);
+                    var commitDate = commit.Author.When.Date.ToString().Replace(" 00:00:00", "");
+                    if(commitDict.ContainsKey(commitDate)) {
+                        commitDict[commitDate] += 1;
+                    } else {
+                        commitDict.Add(commitDate, 1);
+                    }
                 }
+                listCommit = commitDict.Select(pair => new String(pair.Value + " " + pair.Key)).ToList();
             }
-            return new List<String>();
+            return listCommit;
         }
         throw new ArgumentException();
     }
--- ApplicationTest/UnitTest1.cs
+++ ApplicationTest/UnitTest1.cs
@@ -7,15 +7,19 @@ public class UnitTest1
     public void TestCommitFrequencyMode()
     {
         // Arrange
-        var path = "GITHUBPATH";
+        var path = @"C:\Users\chris\OneDrive\Skrivebord\GIthubRepo\assignment-05";
         var excepted = new List<String>() {
-            "1 2017-12-08",
-            "6 2017-12-26",
-            "12 2018-01-01",
-            "13 2018-01-02",
-            "10 2018-01-14",
-            "7 2018-01-17",
-            "5 2018-01-18"
+            "5 11-10-2022",
+            "2 10-10-2022",
+            "2 07-10-2022",
+            "3 06-10-2022",
+            "2 25-09-2022",
+            "2 21-10-2021",
+            "3 15-10-2021",
+            "2 04-10-2021",
+            "1 03-10-2021",
+            "1 02-10-2021",
+            "1 24-09-2021"
         };
 
         // Act
