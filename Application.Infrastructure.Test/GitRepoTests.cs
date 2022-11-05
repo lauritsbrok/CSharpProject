59d90be17eeda05e526cdb59a9a4f6c390f9c00f
Tjorne71 <emtj@itu.dk>
2022-11-05 15:42:59 +0100

    Renamed classes to Git-

--- Application.Infrastructure.Test/GitRepoTests.cs
+++ Application.Infrastructure.Test/GitRepoTests.cs
@@ -9,25 +9,25 @@ public class GitRepoRepositoryTests
     private readonly GitInsightContext _context;
     private readonly GitAuthorRepository _repository;
     
-    public GitRepoRepositoryTests()
-    {
-        _connection = new SqliteConnection("Filename=:memory:");
-        _connection.Open();
-        var builder = new DbContextOptionsBuilder<GitInsightContext>().UseSqlite(_connection);
-        _context = new GitInsightContext(builder.Options);
-        _context.Database.EnsureCreated();
+    // public GitRepoRepositoryTests()
+    // {
+    //     _connection = new SqliteConnection("Filename=:memory:");
+    //     _connection.Open();
+    //     var builder = new DbContextOptionsBuilder<GitInsightContext>().UseSqlite(_connection);
+    //     _context = new GitInsightContext(builder.Options);
+    //     _context.Database.EnsureCreated();
 
-        var TestGitRepo = new GitRepo("TestRepo.com") {Id = 1};
+    //     var TestGitRepo = new GitRepo("TestRepo.com") {Id = 1};
 
-        _context.Authors.AddRange(bob, tim);
-        _context.SaveChanges();
+    //     _context.Authors.AddRange(bob, tim);
+    //     _context.SaveChanges();
 
-        _repository = new GitAuthorRepository(_context);
-    }
+    //     _repository = new GitAuthorRepository(_context);
+    // }
 
-    [Fact]
-    public void Create()
-    {
+    // [Fact]
+    // public void Create()
+    // {
 
-    }
+    // }
 }
