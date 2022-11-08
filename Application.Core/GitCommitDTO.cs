47f6f5feb6129da052bf195f4592678dd1fbf253
Laurits Brok <lauritsbrok@gmail.com>
2022-11-08 12:16:55 +0100

    Added test

--- Application.Core/GitCommitDTO.cs
+++ Application.Core/GitCommitDTO.cs
@@ -1,7 +1,7 @@
 namespace Application.Core;
 
-public record GitCommitDTO(string Id, string Message, string AuthorName, string RepoUrl);
+public record GitCommitDTO(string Id, string Message, GitAuthorDTO Auhtor, GitRepoDTO Repo);
 
-public record GitCommitCreateDTO(string Name, string AuthorName, string RepoUrl);
+public record GitCommitCreateDTO(string Id, string Message, GitAuthorCreateDTO Author, GitRepoDTO Repo);
 
-public record GitCommitUpdateDTO(string Id, string Message, string AuthorName, string RepoUrl);
+public record GitCommitUpdateDTO(string Id, string Message, GitAuthorDTO Auhtor, GitRepoDTO Repo);
--- Application.Infrastructure/GitCommitRepository.cs
+++ Application.Infrastructure/GitCommitRepository.cs
@@ -9,9 +9,24 @@ public class GitCommitRepository : IGitCommitRepository
         _context = context;
     }
 
-    public (Response Response, int CommitId) Create(GitCommitCreateDTO commit)
+    public (Response Response, string commitId) Create(GitCommitCreateDTO commit)
     {
-        throw new NotImplementedException();
+        var entity = _context.Commits.FirstOrDefault(g => g.Id == g.Message);
+        Response response;
+        if(entity is null) {
+            entity = new GitCommit(commit.Id, commit.Message);
+            try {
+                _context.Commits.Add(entity);
+                _context.SaveChanges();
+                response = Response.Created;
+            } catch (Microsoft.EntityFrameworkCore.DbUpdateException) {
+                response = Response.Conflict;
+            }
+        } else {
+            response = Response.Conflict;
+        }
+
+        return (response, entity.Id);
     }
 
     public Response Delete(int commitId, bool force = false)
@@ -19,7 +34,17 @@ public class GitCommitRepository : IGitCommitRepository
         throw new NotImplementedException();
     }
 
+    public Response Delete(string commitId, bool force = false)
+    {
+        throw new NotImplementedException();
+    }
+
     public GitCommitDTO? Find(int commitId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public GitCommitDTO? Find(string commitId)
     {
         throw new NotImplementedException();
     }
